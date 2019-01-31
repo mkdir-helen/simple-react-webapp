bb55bf06fd999236e93979e55d87fb21ef1622d5
mkdir-helen <experiment.helen@gmail.com>
2019-01-30 21:54:34 -0500

    initial set up plus the env for api_key

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
+require('dotenv').config();
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-
+import Titles from "./components/Titles";
+import Form from "./components/Form";
+import Weather from './components/Weather';
 class App extends Component {
+  getWeather = async () => {
+    const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${process.env.REACT_APP_API_KEY}&units=metric`);
+    const data = await api_call.json();
+  }
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <Titles />
+        <Form />
+        <Weather />
       </div>
     );
   }
