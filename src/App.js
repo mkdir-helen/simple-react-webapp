d70aea73a92da77aa798b494be0ef722e211332e
mkdir-helen <experiment.helen@gmail.com>
2019-01-31 16:03:47 -0500

    Added Forms

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
-require('dotenv').config();
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Titles from "./components/Titles";
 import Form from "./components/Form";
 import Weather from './components/Weather';
+require('dotenv').config();
 class App extends Component {
-  getWeather = async () => {
+  getWeather = async (e) => {
+    e.preventDefault();
+    const city = e.target.elements.city.value;
+    const country = e.target.elements.country.value;
     const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${process.env.REACT_APP_API_KEY}&units=metric`);
     const data = await api_call.json();
   }
@@ -14,7 +17,7 @@ class App extends Component {
     return (
       <div className="App">
         <Titles />
-        <Form />
+        <Form getWeather={this.getWeather} />
         <Weather />
       </div>
     );
--- src/components/Form.js
+++ src/components/Form.js
@@ -3,9 +3,11 @@ import React, { Component } from 'react'
 export default class Form extends Component {
     render() {
         return (
-            <div>
-                Form Component
-      </div>
+            <form onSubmit={this.props.getWeather}>
+                <input type="text" name="city" id="City..." />
+                <input type="text" name="country" id="Country..." />
+                <button>Get weather</button>
+            </form>
         )
     }
 }
