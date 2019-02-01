01da99cbd7c68e70e06d765f4bd16bc96cc2a47f
mkdir-helen <experiment.helen@gmail.com>
2019-02-01 17:48:42 -0500

    Added data setState

--- src/App.js
+++ src/App.js
@@ -4,21 +4,47 @@ import './App.css';
 import Titles from "./components/Titles";
 import Form from "./components/Form";
 import Weather from './components/Weather';
-require('dotenv').config();
+
 class App extends Component {
+  state = {
+    temperature: undefined,
+    city: undefined,
+    country: undefined,
+    humidity: undefined,
+    description: undefined,
+    error: undefined
+  }
   getWeather = async (e) => {
     e.preventDefault();
     const city = e.target.elements.city.value;
     const country = e.target.elements.country.value;
-    const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${process.env.REACT_APP_API_KEY}&units=metric`);
+    const API_KEY = `${process.env.REACT_APP_API_KEY}`;
+    console.log(API_KEY);
+    const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&APPID=${API_KEY}&units=imperial`);
     const data = await api_call.json();
+    console.log(data);
+    // this.setState({
+    //   temperature: data.main.temp,
+    //   city: data.name,
+    //   country: data.sys.country,
+    //   humidity: data.main.humidity,
+    //   description: data.weather[0].description,
+    //   error: ""
+    // });
   }
   render() {
     return (
       <div className="App">
         <Titles />
         <Form getWeather={this.getWeather} />
-        <Weather />
+        <Weather
+          temperature={this.state.temperature}
+          city={this.state.city}
+          country={this.state.country}
+          humidity={this.state.humidity}
+          description={this.state.description}
+          error={this.state.error}
+        />
       </div>
     );
   }
--- src/components/Weather.js
+++ src/components/Weather.js
@@ -4,8 +4,12 @@ export default class Weather extends Component {
     render() {
         return (
             <div>
-                Weather
-      </div>
+                {this.props.city}
+                {this.props.country}
+                {this.props.temperature}
+                {this.props.humidity}
+                {this.props.description}
+            </div>
         )
     }
 }
