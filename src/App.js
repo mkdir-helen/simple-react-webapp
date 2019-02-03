0fc515c60a3247edefd521bf33f3042862801a03
mkdir-helen <experiment.helen@gmail.com>
2019-02-03 16:44:47 -0500

    render out the results and errors

--- src/App.js
+++ src/App.js
@@ -18,19 +18,29 @@ class App extends Component {
     e.preventDefault();
     const city = e.target.elements.city.value;
     const country = e.target.elements.country.value;
-    const API_KEY = `${process.env.REACT_APP_API_KEY}`;
-    console.log(API_KEY);
+    const API_KEY = process.env.REACT_APP_API_KEY;
     const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&APPID=${API_KEY}&units=imperial`);
     const data = await api_call.json();
-    console.log(data);
-    // this.setState({
-    //   temperature: data.main.temp,
-    //   city: data.name,
-    //   country: data.sys.country,
-    //   humidity: data.main.humidity,
-    //   description: data.weather[0].description,
-    //   error: ""
-    // });
+    if (city && country) {
+      console.log(data);
+      this.setState({
+        temperature: data.main.temp,
+        city: data.name,
+        country: data.sys.country,
+        humidity: data.main.humidity,
+        description: data.weather[0].description,
+        error: ""
+      });
+    } else {
+      this.setState({
+        temperature: undefined,
+        city: undefined,
+        country: undefined,
+        humidity: undefined,
+        description: undefined,
+        error: "Please enter the value."
+      });
+    }
   }
   render() {
     return (
