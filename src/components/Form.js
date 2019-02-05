91bcd16d4e7e28432c0e99ad99c5b8f8dac764e2
mkdir-helen <experiment.helen@gmail.com>
2019-02-05 09:50:31 -0500

    changed rcc to rfc

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,13 +1,12 @@
 import React, { Component } from 'react'
 
-export default class Form extends Component {
-    render() {
-        return (
-            <form onSubmit={this.props.getWeather}>
-                <input type="text" name="city" id="City..." />
-                <input type="text" name="country" id="Country..." />
-                <button>Get weather</button>
-            </form>
-        )
-    }
-}
+const Form = props => (
+    <form onSubmit={props.getWeather}>
+        <input type="text" name="city" id="City..." />
+        <input type="text" name="country" id="Country..." />
+        <button>Get weather</button>
+    </form>
+);
+
+
+export default Form;
